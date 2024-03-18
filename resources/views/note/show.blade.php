df6c89fcf286214b49c00ffa0fb7e9cd78df510b
Brian Chebon <brianlchebon@gmail.com>
2024-03-18 12:59:50 +0300

    added the css file and also the note components

---
+++ resources/views/note/show.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="note-container single-note">
+        <div class="note-header">
+            <h1 class="text-3xl py-4">Note: {{ $note->created_at }}</h1>
+            <div class="note-buttons">
+                <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+            </div>
+        </div>
+        <div class="note">
+            <div class="note-body">
+                {{ $note->note }}
+            </div>
+        </div>
+    </div>
+</x-layout>
